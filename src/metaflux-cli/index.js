66cb987b4b6137b700690e3141f0599d3ecb34e7
Osmar Reyes <osmarreyesst@gmail.com>
2021-01-08 00:13:12 -0500

    change create to be a sub-command insted of an option

--- src/metaflux-cli/index.js
+++ src/metaflux-cli/index.js
@@ -1,25 +1,11 @@
 (() => {
-	const _options = require('./options');
-	const { writeTemplate } = require('../lib');
+	const createSubCommand = require('./create');
+
 
 	function metafluxCli(program) {
-		const commandObject = program
-		.description('Complete Metaflux project manager, see the usage with metaflux-cli --help')
-		//.usage('[create, dir]');
-
-		_options.forEach(option => {
-			commandObject.option(...option.command);
-		});
-
-		commandObject.action(async () => {
-			let { create, dir } = commandObject;
-			if (create) {
-				create = typeof create === 'boolean' ? undefined : create;
-				console.log(create);
-				await writeTemplate(dir, create)
-			}
-			process.exit(0);
-		});
+		program.description('Complete Metaflux project manager, see the usage with metaflux-cli --help')
+		// create sub command
+		createSubCommand(program);
 	}
 
 	module.exports = metafluxCli;
