078ae2539cd8a07c61f55e390b38a043e38f4737
Osmar Reyes <osmarreyesst@gmail.com>
2021-02-08 22:28:09 -0500

    add --silent option

--- src/metaflux-cli/run/index.js
+++ src/metaflux-cli/run/index.js
@@ -1,24 +1,31 @@
 (() => {
 	const shell = require('shelljs');
 	const fs = require('fs-extra');
+	const options = require('./options');
 	/**
 	 * Start Dev server sub-command
 	 */
 	function run (program) {
 		const commandObject = program
+		options.forEach(option => {
+			commandObject.option(...option.command);
+		});
 		commandObject.command('run')
 		.description('Run your project in Dev mode')
 		.action(async function () {
+			const { silent } = commandObject;
 			if(!fs.existsSync('node_modules/')) {
 				console.log('#>- Installing dependencies, this may take a couple of minutes');
 				await shell.exec('npm install', { silent: true });
 				console.log('#>- Done')
 			}
 			const {stdout} =  shell.exec('npm start', { silent: true, async: true });
-			console.log('#>- Start / watch Dev Server');
-			stdout.on('data', (chunk) => {
-				console.log(chunk);
-			});
+			console.log('#>- Start / watch Dev Server you can end it with (ctrl + c)');
+			if (!silent) {
+				stdout.on('data', (chunk) => {
+					console.log(chunk);
+				});
+			}
 		});
 	}
 
---
+++ src/metaflux-cli/run/options.js
@@ -0,0 +1,7 @@
+// Options description
+module.exports = [
+	{
+		name: 'silent',
+		command: ['-s, --silent', 'Subcommad run will not display the watch info']
+	}
+]
