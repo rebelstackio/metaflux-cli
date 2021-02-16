28da9153fe1ea4a999e0f650de1b61fca77d7069
Osmar Reyes <osmarreyesst@gmail.com>
2021-02-15 19:43:55 -0500

    add full testing / coverage to new feature

--- src/metaflux-cli/run/index.spec.js
+++ src/metaflux-cli/run/index.spec.js
@@ -1,12 +1,17 @@
 const run = require('./index');
 const shell = require('shelljs');
-
+const fs = require('fs-extra');
+// mock dependencies
 jest.mock('shelljs');
+jest.mock('fs-extra');
 shell.exec = jest.fn();
+fs.existsSync = jest.fn();
+const existsSync = fs.existsSync;
 const execMock = shell.exec;
 
 beforeEach(() => {
 	execMock.mockReset();
+	existsSync.mockReset();
 });
 
 function cli(silent) {
@@ -23,8 +28,25 @@ function cli(silent) {
 }
 
 it('should call npm start if dependencies are installed', () => {
+	// this means it has node_modules folder;
+	existsSync.mockReturnValueOnce(true);
+
 	cli(true);
-	//expect(resp).toEqual(null);
+	expect(existsSync).toHaveBeenCalledTimes(1);
+	expect(existsSync).toHaveBeenCalledWith('node_modules/');
+	///
 	expect(execMock).toHaveBeenCalledTimes(1);
 	expect(execMock).toHaveBeenCalledWith('npm start', {"silent": true});
+});
+
+it('should call npm install if dependencies aren\'t installed', () => {
+	// this means it doesn't have node_modules folder;
+	existsSync.mockReturnValueOnce(false);
+
+	cli(true);
+	expect(existsSync).toHaveBeenCalledTimes(1);
+	expect(existsSync).toHaveBeenCalledWith('node_modules/');
+	///
+	expect(execMock).toHaveBeenCalledTimes(1);
+	expect(execMock).toHaveBeenCalledWith('npm install', {"silent": true});
 });
